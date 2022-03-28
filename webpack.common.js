0c838d042daaad1e84621b2751a01b762d2e8ec2
wisdom132 <wisdomekpot@gmail.com>
2022-03-28 03:19:48 +0100

    minifier

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: {
         main: "./src/index.js",
         vendor: "./src/vendor.js"
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html",
-        minify:true
-    })],
+   
     module: {
         rules: [
            
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -13,7 +14,14 @@ module.exports = merge(common, {
         path: path.resolve(__dirname, "bundled")
     },
     optimization: {
-        minimizer: [new CssMinimizerPlugin(), new TerserPlugin(), ]
+        minimizer: [new CssMinimizerPlugin(), new TerserPlugin(), new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                // removeComments: true
+            }
+        }) ]
     },
     plugins: [ new MiniCssExtractPlugin(
     {
